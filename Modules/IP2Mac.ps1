1438dbdc051e550899832433a701da5954451176
Jan Kristensen <jankristensen@outlook.com>
2026-04-28 08:17:40 +0200

    Create IP2Mac.ps1

---
+++ Modules/IP2Mac.ps1
@@ -0,0 +1,13 @@
+﻿# Convert IP Address to Mac Address, using known prefeix
+# - Ref : https://ss64.com/ps/syntax-f-operator.html
+# ------------------------------------------------------------
+Function IP2Mac {
+    param (
+        [cmdletbinding()]
+        [Parameter(Mandatory)][string]$IpAddress
+    )
+
+    $ip = $IpAddress.Split('.')
+    $mac = "BC-24-{0:X2}-{1:X2}-{2:X2}-{3:X2}" -f [int]$ip[0], [int]$ip[1], [int]$ip[2], [int]$ip[3]
+    return $mac
+}
