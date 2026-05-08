54704cb3043132e0eebd8d9100f65dd1f8d81a06
Jan Kristensen <jankristensen@outlook.com>
2026-05-08 11:30:37 +0200

    Update ADDS-Tiering.ps1

--- BuildScripts/Custom/Active Directory/ADDS-Tiering.ps1
+++ BuildScripts/Custom/Active Directory/ADDS-Tiering.ps1
@@ -20,135 +20,114 @@ if ($PSScriptRoot -and $PSScriptRoot -ne "") {
 }
 
 
-# Get Required Files from GIT
+# Deployment server Name and Address
 # ------------------------------------------------------------
-$GitConnection = Get-Content -Path "$RootPath\GitHub-Connection.json" | Convertfrom-Json
+if (-NOT (Test-Path -Path "$RootPath\DeploymentServer.json")) {
+    Write-Warning "Missing Deployment server connection information"
+    Start-Sleep -Seconds 600
+}
+
+$Deployment = Get-Content -Path "$RootPath\DeploymentServer.json" | Convertfrom-Json
+try {
+    Resolve-DnsName $Deployment.ServerName -QuickTimeout -ErrorAction Stop | Out-Null
+    $DeploymentServer = $Deployment.ServerName
+}
+catch {
+    $DeploymentServer = $Deployment.IpAddress
+}
 
 
-if ($($GitConnection.Token)) {
-    $Response = Invoke-RestMethod -Uri "$($GitConnection.Url)/LAB-Infrastructure/contents/Tools/ADTiering.zip" -Headers @{ Authorization = "token $($GitConnection.Token)" }
+if (-not (Test-NetConnection $DeploymentServer -CommonTCPPort HTTP -InformationLevel Quiet)) {
+    throw "Unable to connect to Deployment Website"
 } else {
-    $Response = Invoke-RestMethod -Uri "$($GitConnection.Url)/LAB-Infrastructure/contents/Tools/ADTiering.zip"
+    $RepoUrl  = "http://$DeploymentServer/$($Deployment.VirtualPath)"
 }
-$FileBytes = [System.Convert]::FromBase64String($Response.content)
-[System.IO.File]::WriteAllBytes("$PSScriptRoot\$($Response.name)", $FileBytes)
 
 
-# Setup Active Directory Tiering.
+# Wait until AD Tiering file is copied.
 # ------------------------------------------------------------
-if (Test-Path -Path "$PSScriptRoot\$($Response.name)") {
-
-
-    # Extract the TS AD Tiering Package
-    # ------------------------------------------------------------
-    Expand-Archive -Path "$PSScriptRoot\$($Response.name)" -DestinationPath $PSScriptRoot -Force
-
-
-    # Find the AD Tiering Scripts
-    # ------------------------------------------------------------
-    $ADTieringScript = Get-ChildItem -Path $PSScriptRoot -Recurse -Include "Deploy-TSxADTiering.ps1"
-
-
-    # Setup TS AD Tiering
-    # ------------------------------------------------------------
-    Remove-Item -Path (Get-ChildItem -Path $($ADTieringScript.Directory.FullName) -Filter "*.csv").fullname
-
-#    & $($ADTieringScript.FullName) -CompanyName "$((Get-ADDomain).Name) Endpoints" -TierOUName "Admin" -NoOfTiers 1 -SkipTierEndpointsPAW -SkipTierEndpoints -SkipComputerRedirect -WindowsLAPSOnly
-    & $($ADTieringScript.FullName) -CompanyName "$((Get-ADDomain).Name) Endpoints" -TierOUName "Admin" -NoOfTiers 2 -WindowsLAPSOnly
-
-
-    # Import TS Tiering Module
-    # ------------------------------------------------------------
-    Import-Module (Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "TSxTieringModule.psm1").FullName -Force
-
-
-    # List all OrganizationalUnits used in Domain JSON files.
-    # ------------------------------------------------------------
-    $DomainInfo = $(Get-ADDomain)
-
-    $MACFilter = @("00", "00") + ((((Get-NetAdapter | Where {$_.Status -eq "UP"} | Select-Object -First 1).MacAddress) -Split("-"))[2..4]) -Join("-")
-
-    $URL = "$($GitConnection.Url)/LAB-Deployment/contents/ConfigFiles/JSON"
-
-    if ($($GitConnection.Token)) {
-        $FLResponse = Invoke-RestMethod -Uri $URL -Headers @{ Authorization = "token $($GitConnection.Token)" } -ErrorAction Stop
-    } else {
-        $FLResponse = Invoke-RestMethod -Uri $URL -ErrorAction Stop
-    }
-    $GitJsonFiles = $FLResponse.Name | % { ($_ -split("\."))[0] } | Where {$_ -like "$MACFilter*"}
-
-    $CreateOUs = New-Object System.Collections.ArrayList
-    $CreateServers = New-Object System.Collections.ArrayList
-
-    $GitJsonFiles | Foreach {
-        if ($($GitConnection.Token)) {
-            $DLResponse = Invoke-RestMethod -Uri "$($GitConnection.Url)/LAB-Deployment/contents/ConfigFiles/JSON/$($_).json" -Headers @{ Authorization = "token $($GitConnection.Token)" } -ErrorAction Stop
+if (-NOT (Test-Path -Path "$RootPath\ADTiering.zip")) {
+    Write-Output "Please copy the Truesec Active Directory Tiering file to $RootPath"
+    for ($i=0; $i -lt 9999; $i++) {
+        if (Test-Path -Path "$RootPath\ADTiering.zip") {
+            break
         } else {
-            $DLResponse = Invoke-RestMethod -Uri "$($GitConnection.Url)/LAB-Deployment/contents/ConfigFiles/JSON/$($_).json" -ErrorAction Stop
-        }
-        $FileBytes = [System.Convert]::FromBase64String($DLResponse.content)
-        $JsonText = ([System.Text.Encoding]::UTF8.GetString($FileBytes)).TrimStart([char]0xFEFF)
-        $ServerData = $JsonText | ConvertFrom-Json
-
-        if ($ServerData.JoinOptions.OrganizationalUnit) {
-            $OrganizationalUnit = @($($ServerData.JoinOptions.OrganizationalUnit), $($DomainInfo.DistinguishedName)) -join(",")
-            $CreateServers += [PSCustomObject]@{ Name = $($ServerData.name); Path = $OrganizationalUnit }
-            
-            try {
-                $OUTest = Get-ADOrganizationalUnit -Identity $OrganizationalUnit
-            }
-            Catch {
-
-                $OUParts = $OrganizationalUnit -split(",")
-
-                $OUName = $OUParts[0] -replace '^OU='
-
-                $AdminOUIndex = $OUParts.IndexOf('OU=Admin')
-                if ($AdminOUIndex -gt 0) { $OUTier = $OUParts[$AdminOUIndex - 1] -replace '^OU=' }
-
-                $CreateOUs += $([PSCustomObject]@{ Name = $OUName; Tier = $OUTier })
-            }
+            Start-Sleep -Seconds 30
         }
     }
-
-
-    # Create all OUs listed in Domain JSON files.
-    # ------------------------------------------------------------
-    $CreateOUs | Sort-Object -Unique -Property Name | Foreach {
-        New-TSxSubOU -Tier $_.Tier -Name $_.Name -Description "$($_Tier) $($_.Name)" -TierOUName "Admin" -CompanyName "Dev Endpoints" -WindowsLAPSOnly -Cleanup
-    }
-    
-
-    # Get Data from Json Config
-    # ------------------------------------------------------------
-    If (Test-Path -Path "$PSScriptRoot\ServerConfig.json") {
-        $ServerConfig = Get-Content -Path "$PSScriptRoot\ServerConfig.json" | ConvertFrom-Json
-    }
-
-
-    # Create Domain Join Group and Account.
-    # ------------------------------------------------------------
-    if (-Not(Get-ADOrganizationalUnit -Filter "Name -eq 'ComputerQuarantine'")) {
-        $NewOU = New-TSxADOrganizationalUnit -Name "ComputerQuarantine" -Path "OU=Admin,$($DomainInfo.DistinguishedName)"
-        & redircmp.exe $NewOU.DistinguishedName
-    }
-
-    $DomainJoinPassword = ConvertTo-SecureString -string $ServerConfig.JoinOptions.Password -AsPlainText -Force
-    New-ADUser -Name $ServerConfig.JoinOptions.Username -AccountPassword $DomainJoinPassword -Enabled $true -Path "OU=ServiceAccounts,OU=Tier0,OU=Admin,$($DomainInfo.DistinguishedName)"
-    
-    New-ADGroup -Name "Delegate Control - Join Computers" -Path "OU=Groups,OU=Tier0,OU=Admin,$($DomainInfo.DistinguishedName)" -GroupScope Global
-    Add-ADGroupMember -Identity "Delegate Control - Join Computers" -Members $ServerConfig.JoinOptions.Username
-
-    $ADGroup = Get-ADGroup -Identity "Delegate Control - Join Computers"
-
-    @((Get-ADOrganizationalUnit -Filter "name -eq 'ComputerQuarantine'").DistinguishedName
-        (Get-ADOrganizationalUnit -Filter "name -eq 'JumpStationsLimited'").DistinguishedName
-        (Get-ADOrganizationalUnit -Filter "name -eq 'JumpStations'").DistinguishedName
-        (Get-ADOrganizationalUnit -Filter "name -eq 'Servers'").DistinguishedName
-    ) | Foreach { Set-TSxOUPermission -OrganizationalUnitDN $_ -GroupName $ADGroup.Name -ObjectType ComputersCreate }
-
 }
 
+
+# Extract Tiering Package
+# ------------------------------------------------------------
+Expand-Archive -Path "$RootPath\ADTiering.zip" -DestinationPath $RootPath -Force
+
+
+# Find the AD Tiering Scripts
+# ------------------------------------------------------------
+$ADTieringScript = Get-ChildItem -Path $RootPath -Recurse -Include "Deploy-TSxADTiering.ps1"
+
+
+# Setup TS AD Tiering
+# ------------------------------------------------------------
+Remove-Item -Path (Get-ChildItem -Path $($ADTieringScript.Directory.FullName) -Filter "*.csv").fullname
+
+& $($ADTieringScript.FullName) -CompanyName "$((Get-ADDomain).Name) Endpoints" -TierOUName "Admin" -NoOfTiers 1 -SkipTierEndpointsPAW -SkipTierEndpoints -SkipComputerRedirect -WindowsLAPSOnly
+#    & $($ADTieringScript.FullName) -CompanyName "$((Get-ADDomain).Name) Endpoints" -TierOUName "Admin" -NoOfTiers 2 -WindowsLAPSOnly
+
+
+# Import TS Tiering Module
+# ------------------------------------------------------------
+Import-Module (Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "TSxTieringModule.psm1").FullName -Force
+
+
+# List all OrganizationalUnits used in Domain JSON files.
+# ------------------------------------------------------------
+$DomainInfo = $(Get-ADDomain)
+
+$MACFilter = @("00", "00") + ((((Get-NetAdapter | Where {$_.Status -eq "UP"} | Select-Object -First 1).MacAddress) -Split("-"))[2..4]) -Join("-")
+
+$URL = "$RepoUrl/ConfigFiles/JSON"
+
+$FLResponse = Invoke-WebRequest -Uri $URL -ErrorAction Stop -UseBasicParsing
+$GitJsonFiles = ($FLResponse.Links.href | Where {$_ -like "*$MACFilter*"}) -replace("/$($Deployment.VirtualPath)","")
+
+$CreateOUs = New-Object System.Collections.ArrayList
+$CreateServers = New-Object System.Collections.ArrayList
+
+$GitJsonFiles[1] | Foreach {
+
+    Invoke-WebRequest -Uri "$RepoUrl/$($_)" -UseBasicParsing -OutFile "$($ENV:TEMP)\JsonConvert.json"
+    $ServerData = Get-Content -Path "$($ENV:TEMP)\JsonConvert.json" -Raw -Encoding UTF8 | ConvertFrom-Json
+
+    if ($ServerData.JoinOptions.OrganizationalUnit) {
+        $OrganizationalUnit = @($($ServerData.JoinOptions.OrganizationalUnit), $($DomainInfo.DistinguishedName)) -join(",")
+        $CreateServers += [PSCustomObject]@{ Name = $($ServerData.name); Path = $OrganizationalUnit }
+            
+        try {
+            $OUTest = Get-ADOrganizationalUnit -Identity $OrganizationalUnit
+        }
+        Catch {
+
+            $OUParts = $OrganizationalUnit -split(",")
+
+            $OUName = $OUParts[0] -replace '^OU='
+
+            $AdminOUIndex = $OUParts.IndexOf('OU=Admin')
+            if ($AdminOUIndex -gt 0) { $OUTier = $OUParts[$AdminOUIndex - 1] -replace '^OU=' }
+
+            $CreateOUs += $([PSCustomObject]@{ Name = $OUName; Tier = $OUTier })
+        }
+    }
+}
+
+
+# Create all OUs listed in Domain JSON files.
+# ------------------------------------------------------------
+$CreateOUs | Sort-Object -Unique -Property Name | Foreach {
+    New-TSxSubOU -Tier $_.Tier -Name $_.Name -Description "$($_Tier) $($_.Name)" -TierOUName "Admin" -CompanyName "Dev Endpoints" -WindowsLAPSOnly -Cleanup
+}
+    
 
 # Get Data from Json Config
 # ------------------------------------------------------------
@@ -157,9 +136,47 @@ If (Test-Path -Path "$PSScriptRoot\ServerConfig.json") {
 }
 
 
+# Create Domain Join Group and Account.
+# ------------------------------------------------------------
+if (-Not(Get-ADOrganizationalUnit -Filter "Name -eq 'Quarantine'")) {
+    $NewParrentOU  = New-TSxADOrganizationalUnit -Name "Quarantine" -Path "$($DomainInfo.DistinguishedName)"
+    
+    $ParrentOU = (Get-ADOrganizationalUnit $($NewParrentOU.DistinguishedName)).DistinguishedName
+    $NewComputerOU = New-TSxADOrganizationalUnit -Name "Computers"  -Path $ParrentOU
+    $NewUserOU     = New-TSxADOrganizationalUnit -Name "Users"      -Path $ParrentOU
+
+    & redircmp.exe $NewComputerOU.DistinguishedName
+    & redircmp.exe $NewUserOU.DistinguishedName
+    
+    <#
+    # Cleanup
+    & redircmp.exe "CN=Computers,$($DomainInfo.DistinguishedName)"
+    & redircmp.exe "CN=Users,$($DomainInfo.DistinguishedName)"
+    
+    Get-ADOrganizationalUnit -Filter * -SearchBase $NewParrentOU | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false
+    Get-ADOrganizationalUnit -Identity $NewComputerOU | Remove-ADOrganizationalUnit -Recursive -Confirm:$False
+    #>
+
+}
+
+$DomainJoinPassword = ConvertTo-SecureString -string $ServerConfig.JoinOptions.Password -AsPlainText -Force
+New-ADUser -Name $ServerConfig.JoinOptions.Username -AccountPassword $DomainJoinPassword -Enabled $true -Path "OU=ServiceAccounts,OU=Tier0,OU=Admin,$($DomainInfo.DistinguishedName)"
+    
+New-ADGroup -Name "Delegate Control - Join Computers" -Path "OU=Groups,OU=Tier0,OU=Admin,$($DomainInfo.DistinguishedName)" -GroupScope Global
+Add-ADGroupMember -Identity "Delegate Control - Join Computers" -Members $ServerConfig.JoinOptions.Username
+
+$ADGroup = Get-ADGroup -Identity "Delegate Control - Join Computers"
+
+@((Get-ADOrganizationalUnit -Filter "name -eq 'ComputerQuarantine'").DistinguishedName
+    (Get-ADOrganizationalUnit -Filter "name -eq 'JumpStationsLimited'").DistinguishedName
+    (Get-ADOrganizationalUnit -Filter "name -eq 'JumpStations'").DistinguishedName
+    (Get-ADOrganizationalUnit -Filter "name -eq 'Servers'").DistinguishedName
+) | Foreach { Set-TSxOUPermission -OrganizationalUnitDN $_ -GroupName $ADGroup.Name -ObjectType ComputersCreate }
+
+
 # Change Task to Completed
 # ------------------------------------------------------------
 if ($ServerConfig) {
-    ($ServerConfig.Tasks | Where-Object { $_.Name -eq "ADDS-01.ps1" }).status = "Completed"
+    ($ServerConfig.Tasks | Where-Object { $_.Name -like "*$(Split-Path $PSCommandPath -Leaf)" }).status = "Completed"
     $ServerConfig | ConvertTo-Json -Depth 5 | Out-File -FilePath "$RootPath\ServerConfig.json" -Encoding utf8 -Force
 }
