19441334ca00de78c81ffd934f9c77d7fde0a88e
Jan Kristensen <jankristensen@outlook.com>
2026-05-08 11:30:39 +0200

    Update ADDS-InitialUsers.ps1

--- BuildScripts/Custom/Active Directory/ADDS-InitialUsers.ps1
+++ BuildScripts/Custom/Active Directory/ADDS-InitialUsers.ps1
@@ -18,6 +18,30 @@ if ($PSScriptRoot -and $PSScriptRoot -ne "") {
 }
 
 
+# Deployment server Name and Address
+# ------------------------------------------------------------
+if (-NOT (Test-Path -Path "$RootPath\DeploymentServer.json")) {
+    Write-Warning "Missing Deployment server connection information"
+    Start-Sleep -Seconds 600
+}
+
+$Deployment = Get-Content -Path "$RootPath\DeploymentServer.json" | Convertfrom-Json
+try {
+    Resolve-DnsName $Deployment.ServerName -QuickTimeout -ErrorAction Stop | Out-Null
+    $DeploymentServer = $Deployment.ServerName
+}
+catch {
+    $DeploymentServer = $Deployment.IpAddress
+}
+
+
+if (-not (Test-NetConnection $DeploymentServer -CommonTCPPort HTTP -InformationLevel Quiet)) {
+    throw "Unable to connect to Deployment Website"
+} else {
+    $RepoUrl  = "http://$DeploymentServer/$($Deployment.VirtualPath)"
+}
+
+
 # Import TS Tiering Module
 # ------------------------------------------------------------
 Import-Module (Get-ChildItem -Path $RootPath -Recurse -Filter "TSxTieringModule.psm1").FullName -Verbose -Force
@@ -30,29 +54,40 @@ If (-NOT (Get-Command -Name New-TSxAdminAccount)) {
 
 # Create Users.
 # --------------------------------------------------------------------------------------------------
-$UserList = @(
-    [PSCustomObject]@{ FirstName = "Jan";              LastName = "Kristensen" }
-    [PSCustomObject]@{ FirstName = "Homer";            LastName = "Simpson"    }
-    [PSCustomObject]@{ FirstName = "Marjorie Bouvier"; LastName = "Simpson"    }
-)
+#$UserList = @(
+#    [PSCustomObject]@{ FirstName = "Jan";              LastName = "Kristensen"; Type = "FTE"; Access = @("T0","T1","T2","Te") }
+#    [PSCustomObject]@{ FirstName = "Homer";            LastName = "Simpson";    Type = "EXT"; Access = @("T1L","T2L")         }
+#    [PSCustomObject]@{ FirstName = "Marjorie Bouvier"; LastName = "Simpson";    Type = "EXT"; Access = @("T1L","T2L")         }
+#)
+#$UserList | ConvertTo-Json
 
+Invoke-WebRequest -Uri "$RepoUrl/ConfigFiles/Initial-Users.json" -UseBasicParsing -OutFile "$($ENV:TEMP)\Initial-Users.json"
+$UserList = Get-Content -Path "$($ENV:TEMP)\Initial-Users.json" | ConvertFrom-Json
 
 $CreatedUsers = @()
-$UserList | ForEach-Object {
-    $CreatedUsers += New-TSxAdminAccount -FirstName $_.FirstName -LastName $_.LastName -AccountType T0  -Prefix "Adm" -Suffix "FTE" -AddToSilo $false
-    $CreatedUsers += New-TSxAdminAccount -FirstName $_.FirstName -LastName $_.LastName -AccountType T1  -Prefix "Adm" -Suffix "FTE" -AddToSilo $false
-    $CreatedUsers += New-TSxAdminAccount -FirstName $_.FirstName -LastName $_.LastName -AccountType CON -Prefix "Adm" -Suffix "FTE" -AddToSilo $false
+ForEach ($User in $UserList) {
+
+    $User.Access | ForEach-Object {
+        if ($_ -eq "TE") { $Silo = $false } else { $Silo = $true }
+        $CreatedUsers += New-TSxAdminAccount -FirstName $User.FirstName -LastName $User.LastName -AccountType $($_) -AddToSilo $Silo -ErrorAction SilentlyContinue
+    }
+    if ($User.type -eq "EXT") {
+        $CreatedUsers += New-TSxAdminAccount -FirstName $User.FirstName -LastName $User.LastName -AccountType CON -AddToSilo $false
+    }
 }
 
 
 # Create Groups.
 # --------------------------------------------------------------------------------------------------
-$GroupList = @(
-    [PSCustomObject]@{ Name = "Access - PingCastle Readers";  Tier = "Tier1"; Description = "Read access to PingCastle Rapports" }
-    [PSCustomObject]@{ Name = "Access - Admin File Share RW"; Tier = "Tier1"; Description = "Read/Write access to IT Admin Share" }
-    [PSCustomObject]@{ Name = "Access - Admin File Share RO"; Tier = "Tier1"; Description = "Read only access to IT Admin Share" }
-)
+#$GroupList = @(
+#    [PSCustomObject]@{ Name = "Access - PingCastle Readers";  Tier = "Tier1"; Description = "Read access to PingCastle Rapports" }
+#    [PSCustomObject]@{ Name = "Access - Admin File Share RW"; Tier = "Tier1"; Description = "Read/Write access to IT Admin Share" }
+#    [PSCustomObject]@{ Name = "Access - Admin File Share RO"; Tier = "Tier1"; Description = "Read only access to IT Admin Share" }
+#)
+#$GroupList | ConvertTo-Json
 
+Invoke-WebRequest -Uri "$RepoUrl/ConfigFiles/Initial-Groups.json" -UseBasicParsing -OutFile "$($ENV:TEMP)\Initial-Groups.json"
+$GroupList = Get-Content -Path "$($ENV:TEMP)\Initial-Groups.json" | ConvertFrom-Json
 
 $GroupList | ForEach-Object {
     $GroupPath = (((Get-ADGroup -Identity "Domain $($_.Tier) Service accounts") -Split(","))[1..99]) -Join(",")
@@ -75,19 +110,16 @@ if (!((Get-WmiObject -Class win32_computersystem).partofdomain)) {
 }
 
 
-# UPDATE !!
-
-
 # Get Data from Json Config
 # ------------------------------------------------------------
-If (Test-Path -Path "$PSScriptRoot\ServerConfig.json") {
-    $ServerConfig = Get-Content -Path "$PSScriptRoot\ServerConfig.json" | ConvertFrom-Json
+If (Test-Path -Path "$RootPath\ServerConfig.json") {
+    $ServerConfig = Get-Content -Path "$RootPath\ServerConfig.json" | ConvertFrom-Json
 }
 
 
 # Change Task to Completed
 # ------------------------------------------------------------
 if ($ServerConfig) {
-    ($ServerConfig.Tasks | Where-Object { $_.Name -eq "ADDS-01.ps1" }).status = "Completed"
+    ($ServerConfig.Tasks | Where-Object { $_.Name -like "*$(Split-Path $PSCommandPath -Leaf)" }).status = "Completed"
     $ServerConfig | ConvertTo-Json -Depth 5 | Out-File -FilePath "$RootPath\ServerConfig.json" -Encoding utf8 -Force
 }
